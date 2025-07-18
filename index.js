759093091f86efa8660f744f9034a4a6ca60e243
Arteomcik9 <moglanarteom@gmail.com>
2025-07-18 11:37:52 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+//JavaScript-код с созданием переменных
+/*Автор
+Артём моглан */
+// переменные
+let myName ="Артём"
+let mySurname ="Моглан"
+let myFavoriteDrink ="Вода"
+let myFavoriteAnimal ="Лиса"
+let myFavoriteProgrammingLanguage ="HTML"
+console.log(myName)
+console.log(mySurname)
+console.log(myFavoriteDrink)
+console.log(myFavoriteAnimal)
+console.log(myFavoriteProgrammingLanguage)
+// car
+const carName = "Toyota"
+let maxCarSpeed = "200km/h"
+let carOwner = "Владимир"
+alert(carName)
+alert(maxCarSpeed)
+alert(carOwner)
+maxCarSpeed = "250km/h"
+console.log('У машины изменилась скорость', maxCarSpeed)
+carOwner = "Максим"
+console.log('У машины изменился владелец', carOwner)
+maxCarSpeed = "233km/h"
+console.log('У машины изменилась скорость', maxCarSpeed)
+carOwner = "Дмитрий"
+console.log('У машины изменился владелец', carOwner)
+maxCarSpeed = "319km/h"
+console.log('У машины изменилась скорость', maxCarSpeed)
+carOwner = "Александр"
+console.log('У машины изменился владелец', carOwner)
+// const
+const question1 = "Какая самая маленькая страна мира по населению?"
+const question2 = "Какая самая большая страна мира по населению?"
+const question3 = "Какой самый большой материк на планете?"
+const answer1 = "Китай"
+const answer2 = "Евразия"
+const answer3 = "Ватикан"
+console.log(question1, 'Ответ:', answer3)
+console.log(question2, 'Ответ:', answer1)
+console.log(question3, 'Ответ:', answer2)
+
+
